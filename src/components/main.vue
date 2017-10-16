ea8eda47e5ca065a9cf65682dfa98fbb49993cae
Sergey <sunliveua@gmail.com>
2017-10-16 13:49:59 +0300

    0.2.1

--- src/components/main.vue
+++ src/components/main.vue
@@ -4,21 +4,25 @@
     <section class="banner">
       <app-banner></app-banner>
     </section>
+    <!-- slider -->
+    <section class="Slider">
+      <app-slider></app-slider>
+    </section>
   </main>
 </template>
 
 <script>
 import banner from './banner'
+import slider from './slider'
 
 
 
   export default {
     components: {
-      'app-banner': banner
+      'app-banner': banner,
+      'app-slider': slider
     }
   }
 </script>
 
-<style scoped lang="stylus">
-
-</style>
+<style scoped></style>
--- src/components/slider.vue
+++ src/components/slider.vue
@@ -1,13 +1,29 @@
 <template>
-  <v-container fluid></v-container>
+  <v-container fluid class="py-5">
+    <carousel-3d autoplay :autoplayTimeout="5000" autoplayHoverPause :width="450" :height="600" :border="2">
+      <slide :index="i" v-for="(item, i) in collection" :key="i">
+        <img :src="item.img" :alt="item.title">
+      </slide>
+    </carousel-3d>
+  </v-container>
 </template>
 
 <script>
   export default {
-    
+    computed: {
+      collection () {
+        return this.$store.state.collection
+      }
+    }
   }
 </script>
 
-<style scoped>
+<style scoped lang="stylus">
+.container
+  min-height 600px
+.carousel-3d-container
+  margin 0
 
+.carousel-3d-slide
+  border-radius 10px
 </style>
