9cf63e2bca0cc9fe9335567887fe8a089ac74526
Sergey <sunliveua@gmail.com>
2017-10-17 10:27:48 +0300

    0.3.2.4

--- src/components/comments.vue
+++ src/components/comments.vue
@@ -1,14 +1,26 @@
 <template>
   <v-container fluid>
-    <v-layout>
+<!--     <v-layout>
       <v-flex xs12 class="test text-xs-center">
-        <img src="/static/birka.png" alt="background">
+        <img src="/static/comments.jpg" alt="background">
         <div class="content">
           <div class="avatar">
             <img src="http://funkyimg.com/i/2ynJE.png" alt="Oksana">
           </div>
         </div>
         <div class="content2">Мы собрали команду молодых дизайнеров, которые предлагают нашим клиентам экслюзивную, модную, удобную, качественную одежду. А с учетом собственной реализации мы предлагаем отличные цены. Наши постоянные клиенты будут приятно удивлены. Рекомендую и Вам убедиться в этом.</div>
+      </v-flex>
+    </v-layout> -->
+    <v-layout justify-center align-center>
+      <v-flex xs12 class="background">
+        <v-flex xs12 sm6 lg4 class="text-xs-center">
+          <v-avatar size="300px">
+            <img src="http://funkyimg.com/i/2ynJE.png" alt="">
+          </v-avatar>
+        </v-flex>
+        <v-flex xs12 sm6 lg4 class="text-xs-center">
+          <div class="content3">Мы собрали команду молодых дизайнеров, которые предлагают нашим клиентам экслюзивную, модную, удобную, качественную одежду. А с учетом собственной реализации мы предлагаем отличные цены. Наши постоянные клиенты будут приятно удивлены. Рекомендую и Вам убедиться в этом.</div>
+        </v-flex>
       </v-flex>
     </v-layout>
   </v-container>
@@ -21,11 +33,24 @@
 </script>
 
 <style scoped lang="stylus">
+
+.background
+  background-image url('../../static/comments.jpg')
+  // background-size 100% 100%
+  background-size 100% auto
+
+  width 100%
+  min-height 900px
+  display flex
+  justify-content center
+  align-items center
+  position relative
+
 .test
   position relative
   img
     width 100%
-    max-height 700px
+    max-height 900px
 
 .avatar
   height 300px
@@ -47,4 +72,11 @@
   padding 1.5rem
   font-size 1.6rem
   color #fff
+
+.content3
+  background-color #F3B020
+  padding 1.5rem
+  font-size 1.6rem
+  color #fff
+  box-shadow 0px 0px 10px 10px #F3B020
 </style>
